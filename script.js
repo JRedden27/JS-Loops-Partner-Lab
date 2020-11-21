5e70dc1cb0d2c47d5cb56abacfce4e8b1e6bb513
ChampionModeVision <hwjnthn@live.com>
2020-11-21 12:08:37 -0500

    here ya go

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-let farenheit = 0;
-let celsius = 0;
-console.log("Celsius", "Farenheit")
-for(celsius = 0; celsius <= 100; celsius++) {
-    farenheit = (celsius * (9/5) + 32);
-    console.log(celsius, farenheit);
+// let farenheit = 0;
+// let celsius = 0;
+// console.log("Celsius", "Farenheit")
+// for(celsius = 0; celsius <= 100; celsius++) {
+//     farenheit = (celsius * (9/5) + 32);
+//     console.log(celsius, farenheit);
+// }
+
+
+let num = Number(prompt("Enter A Number"));
+let total = 0;
+let count= 1;
+while (count <= num){
+    total += count; 
+    count++;
+
+
+
 }
+
+console.log(total);
+
+
+
